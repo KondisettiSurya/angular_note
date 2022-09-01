095942d52c91782f3c924b504d6ebb60c5bccf7d
surya <suryakondisetti741@gmail.com>
2022-09-01 22:25:18 +0100

    This is my angular notes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CardComponent } from './card/card.component';
+import { ChangeComponent } from './change/change.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'card',component:CardComponent},
+  {path:'navbar',component:NavbarComponent},
+  {path:'change', component:ChangeComponent}
+
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardComponent } from './card/card.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AddcartComponent } from './addcart/addcart.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { ChangeComponent } from './change/change.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
+    NavbarComponent,
+    AddcartComponent,
+    ShoppingCartComponent,
+    ChangeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
